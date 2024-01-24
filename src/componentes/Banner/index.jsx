327443121b26ba94440e009c96da3ce860193612
Renato França <renatofranca2881@gmail.com>
2024-01-24 20:35:51 -0300

    inicio do banner

---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Banner.module.css';
+
+export default function Banner() {
+    return (
+        <div className={styles.conteiner}>
+            <h1>Dezembro Promocional</h1>
+        </div>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
-import styles from './Home.module.css';
+import Banner from "src/componentes/Banner";
+import styles from "./Home.module.css";
 
 export default function Home() {
-    return (
-        <>
-            <h1 className={styles.titulo}>aqui jas a home</h1>
-        </>
-    )
+  return (
+    <>
+      <Banner />
+      <h1 className={styles.titulo}>aqui jas a home</h1>
+    </>
+  );
 }
